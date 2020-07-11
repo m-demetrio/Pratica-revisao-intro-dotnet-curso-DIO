35a3d9ea6f30ee55397852309eaefb9bf152a3f7
m-demetrio <m_demetrio@outlook.com>
2020-07-11 17:53:12 -0300

    Iniciando aplicação em C# .Net referente ao exercicio proposto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************************");
+            Console.WriteLine("*    Revisão DIO - ALUNOS                          *");
+            Console.WriteLine("*                                                  *");
+            Console.WriteLine("*        1- Inserir Novo Aluno                     *");
+            Console.WriteLine("*        2- Listar Alunos                          *");
+            Console.WriteLine("*        3- Calcular Média Geral                   *");
+            Console.WriteLine("*                                                  *");
+            Console.WriteLine("****************************************************");
+            Console.WriteLine();
+            Console.Write("Insira a opção desejada: ");
+
+            Console.ReadLine();
+        }
+    }
+}
